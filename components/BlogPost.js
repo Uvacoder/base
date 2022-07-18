cfa2d34c8ea5b34349954018d5ebaaa4700b3739
Parth Desai <desaiparth2000@gmail.com>
2022-07-18 22:26:48 +0530

    Major design changes

--- components/BlogPost.js
+++ components/BlogPost.js
@@ -9,16 +9,13 @@ const BlogPost = ({ post }) => {
       <a>
         <article
           key={post.id}
-          className="mb-4 lg:p-4 md:p-4 p-4 rounded-md lg:hover:bg-gray-200 lg:hover:bg-opacity-40 lg:dark:hover:bg-gray-800 lg:dark:hover:bg-opacity-50 md:hover:bg-gray-200 md:hover:bg-opacity-40 md:dark:hover:bg-gray-800 md:dark:hover:bg-opacity-50 transition duration-400 hover:scale-105"
+          className="mb-4 lg:p-4 md:p-4 p-4 rounded-md hover:bg-gray-300/50 dark:hover:bg-gray-300/10 transition duration-400 hover:scale-105"
         >
           <header className="flex flex-col justify-between md:flex-row md:items-baseline">
             <h2 className="text-lg md:text-xl font-medium mb-2 cursor-pointer text-black dark:text-gray-100 transition duration-500 ease-in-out">
               {post.title}
-              {post.tags && (
-                post.tags.map((tag) => (
-                  <Tag key={tag} text={tag} />
-                ))
-              )}
+              {post.tags &&
+                post.tags.map((tag) => <Tag key={tag} text={tag} />)}
             </h2>
             <time className="flex-shrink-0 text-gray-600 dark:text-gray-400 text-sm">
               {formatDate(
@@ -28,7 +25,7 @@ const BlogPost = ({ post }) => {
             </time>
           </header>
           <main>
-            <p className="hidden md:block leading-8 text-gray-700 dark:text-gray-300">
+            <p className="hidden md:block leading-8 text-gray-700 md:text-justify dark:text-gray-300">
               {post.summary}
             </p>
           </main>
