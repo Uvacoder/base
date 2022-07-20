482aee8942754c2f7ac4dca1bf9ddba912599460
Parth Desai <desaiparth2000@gmail.com>
2022-07-20 15:00:58 +0530

    Bug Fix

--- components/ProjectTag.js
+++ components/ProjectTag.js
@@ -1,4 +1,4 @@
-const Tag = ({ text }) => {
+const ProjectTag = ({ text }) => {
   return (
       <span className="inline-block px-1 align-middle text-gray-500 text-xs font-normal w-fit border border-gray-500 dark:border-gray-500 dark:text-gray-400 dark:hover:border-white hover:border-black mr-2 uppercase transition duration-200 ease-in-out hover:text-gray-900 dark:hover:text-white cursor-default">
         {text}
@@ -6,4 +6,4 @@ const Tag = ({ text }) => {
   )
 }
 
-export default Tag
+export default ProjectTag
