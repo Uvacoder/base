4e50408211e2d47f0bb2bde17beb680f5e237e19
Parth Desai <desaiparth2000@gmail.com>
2022-07-23 21:33:30 +0530

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -12,7 +12,7 @@ const BLOG = {
   darkBackground: '#000',
   path: '', // leave this empty unless you want to deploy Nobelium in a folder
   since: 2022, // If leave this empty, current year will be used.
-  postsPerPage: 7,
+  postsPerPage: 15,
   sortByDate: true,
   showProject: true,
   showAbout: true,
